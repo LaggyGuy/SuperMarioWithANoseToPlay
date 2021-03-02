5cab3a46c9eefee60a8b662502c5c0a992a09a1c
LaggyGuy <78725038+LaggyGuy@users.noreply.github.com>
2021-03-02 16:35:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -27,54 +27,18 @@ scrollbind($(".container"));
 
 //Code here
 
-video = ""
-objects = [];
-
 function preload() {
-    video = createVideo('video.mp4');
-    video.hide()
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
 }
 
 function setup() {
-    canvas = createCanvas(480, 380);
-    canvas.center();
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+	instializeInSetup(mario);
 }
 
 function draw() {
-    image(video, 0, 0, 480, 380);
-    if(status != ""){
-      objectDetector.detect(video, gotResult);
-      for(i = 0;i <= objects.length - 1; i++){
-          document.getElementById("status").innerHTML = "Status: Objects Detected";
-          document.getElementById("count").innerHTML = "No Of Objects: "+objects.length;
-
-          fill("red");
-          noFill();
-          stroke("red");
-          percent = floor(objects[i].confidence * 100);
-          text(objects[i].label+" "+percent+"%", objects[i].x + 15, objects[i].y +15);
-          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
-      }
-    }
-}
-
-function start() {
-    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML = "Status: Detecting objects";
-}
-
-function modelLoaded() {
-    console.log("Model Loaded");
-    video.loop();
-    video.speed(1);
-    video.volume(0);
-}
-
-function gotResult(error, results) {
-    if(error){
-        console.error(error);
-    }else{
-        console.log(results);
-        objects = results;
-    }
+	game()
 }
